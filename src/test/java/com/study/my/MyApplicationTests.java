b607b49a1a7a0de0f4aedc99bf3f088bbac88849
tbs03 <tbs03112@naver.com>
2024-03-07 04:49:20 +0900

    Initial commit

---
+++ src/test/java/com/study/my/MyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.my;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
